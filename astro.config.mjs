b8c17a529195e4a01f363544846484a420eda6f7
Firas-Faisal <officialfiras454@gmail.com>
2026-06-15 01:16:21 +0800

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
 
-  site: 'https://Firas-Faisal.github.io',
-
-  base: '/Firas-Faisal.github.io/'
+  site: 'https://Firas-Faisal.github.io'
 });
